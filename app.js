bb0c1ffa36db80a83834c26ce9bae48b82a64dc6
ter21016 <blackblue58@hotmail.com>
2024-11-11 20:20:27 -0800

    add uri

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app
-  .use(bodyParser.json())
+  .use(body-Parser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
